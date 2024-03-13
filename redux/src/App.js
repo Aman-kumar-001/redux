237989e01b38e33c12d415bde82a53a54697ffae
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-03-13 15:25:47 +0530

    started

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Cart from './component/cart';
 import './App.css';
+import  data from './product';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Cart />
+      <div className='product'>
+
+      </div>
     </div>
   );
 }
---
+++ redux/src/product.js
@@ -0,0 +1,116 @@
+
+const data = {
+  products: [
+    {
+      "id": 1,
+      "name": "Aspirin",
+      "price": 21.50,
+      "url": "https://i.imgur.com/5mktRrY.png"
+    },
+    {
+      "id": 2,
+      "name": "Advil",
+      "price": 2000,
+      "url": "https://i.imgur.com/jqMJE3V.png"
+    },
+    {
+      "id": 3,
+      "name": "Paracetamol",
+      "price": 70,
+      "url": "https://i.imgur.com/KeQ1BpS.png"
+    },
+    {
+      "id": 4,
+      "name": "Benadryl",
+      "price": 90,
+      "url": "https://i.imgur.com/cpXk1WO.png"
+    },
+    {
+      "id": 5,
+      "name": "Zantac",
+      "price": 23.58,
+      "url": "https://i.imgur.com/QyWYO28.png"
+    },
+    {
+      "id": 6,
+      "name": "Prilosec",
+      "price": 800,
+      "url": "https://i.imgur.com/2CLSV82.png"
+    },
+    {
+      "id": 7,
+      "name": "Metformin",
+      "price": 36,
+      "url": "https://i.imgur.com/3lTHcXb.png"
+    },
+    {
+      "id": 8,
+      "name": "Ezetrol",
+      "price": 450,
+      "url": "https://i.imgur.com/k87Qjpy.png"
+    },
+    {
+      "id": 9,
+      "name": "euthyrox",
+      "price": 100,
+      "url": "https://i.imgur.com/umX8PKn.png"
+    },
+    {
+      "id": 10,
+      "name": "lisinopril",
+      "price": 197,
+      "url": "https://i.imgur.com/y8kUfbI.png"
+    },
+    {
+      "id": 11,
+      "name": "Advil",
+      "price": 2000,
+      "url": "https://i.imgur.com/5mktRrY.png"
+    },
+    {
+      "id": 12,
+      "name": "Advil",
+      "price": 2000,
+      "url": "https://i.imgur.com/5mktRrY.png"
+    },
+    {
+      "id": 13,
+      "name": "Aspirin",
+      "price": 21.50,
+      "url": "https://i.imgur.com/5mktRrY.png"
+    },
+    {
+      "id": 14,
+      "name": "Advil",
+      "price": 2000,
+      "url": "https://i.imgur.com/jqMJE3V.png"
+    },
+    {
+      "id": 15,
+      "name": "Paracetamol",
+      "price": 70,
+      "url": "https://i.imgur.com/KeQ1BpS.png"
+    },
+    {
+      "id": 16,
+      "name": "Benadryl",
+      "price": 90,
+      "url": "https://i.imgur.com/cpXk1WO.png"
+    },
+    {
+      "id": 17,
+      "name": "Ezetrol",
+      "price": 450,
+      "url": "https://i.imgur.com/k87Qjpy.png"
+    },
+    {
+      "id": 18,
+      "name": "euthyrox",
+      "price": 100,
+      "url": "https://i.imgur.com/umX8PKn.png"
+    }
+  ]
+};
+
+
+export default  data;
