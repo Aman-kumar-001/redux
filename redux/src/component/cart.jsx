f04e79554b488b773ed585527f8b2e7191bfe256
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-03-17 02:00:19 +0530

    pricing

--- redux/src/component/cart.jsx
+++ redux/src/component/cart.jsx
@@ -5,11 +5,12 @@ import { useSelector } from 'react-redux';
 
 function Cart() {
   const data = useSelector(state => state);
+  const total = data.cart.reduce((a,b) => a + b.price, 0)
   console.log(data);
   return (
     <div className="cart-section">
-      <div>Total Price :-- 1250</div>
-      <div>Total items :-- 0</div>
+      <div>Total Price :-- {total}</div>
+      <div>Total items :-- {data.cart.length}</div>
     </div>
   )
 }
